49b78d00643846d153290daed143c9aef9dbd0bb
Ainsley <hello@ainsley.dev>
2024-11-19 19:05:43 +0000

    feat: Adding uninstall

---
+++ uninstall.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Uninstall
+ *
+ * @package     MonduTradeAccount
+ * @category    Base
+ * @author      ainsley.dev
+ */
+
+// If uninstall not called from WordPress, exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+// Delete plugin-specific options
+delete_option( 'mondu_trade_account_options' );
+delete_option( '_mondu_trade_webhooks_registered' );
+delete_option( '_mondu_webhook_secret' );
